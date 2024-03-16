2b39304937f2be4e1732e03132fd2eaae0704237
lwj <3417645461@qq.com>
2024-03-16 18:41:52 +0800

    test

--- src/main/java/com/itbaizhan/User.java
+++ src/main/java/com/itbaizhan/User.java
@@ -2,6 +2,6 @@ package com.itbaizhan;
 
 public class User {
     public static void main(String[] args) {
-        System.out.println("test git ssss");
+        System.out.println("test git ssss gggg");
     }
 }
